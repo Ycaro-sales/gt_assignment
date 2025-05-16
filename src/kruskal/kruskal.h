92e79337c01ddfd15a7f3f4edf720743503d20c2
Wyvian Valença <wgcv2@ic.ufal.br>
2025-05-16 07:22:37 -0300

    feat: new class for weighted graphs as vector of edges for kruskal algorithm

--- src/kruskal/kruskal.h
+++ src/kruskal/kruskal.h
@@ -2,6 +2,35 @@
 
 #ifndef KRUSKAL_H
 
+typedef int vertex;
+typedef float weight;
+
+typedef struct {
+    vertex v1;
+    vertex v2;
+} edge;
+
+typedef struct {
+    edge e;
+    weight w;
+} weighted_edge;
+
+class weighted_graph_vector
+{
+    public:
+    weighted_graph_vector(int size);
+
+    std::vector<weighted_edge> edges;
+
+    void addEdge(vertex v, vertex u, weight w);
+    int size_edges();
+    int size_vertices();
+    int graph_weight();
+    void print_graph();
+    void print_edges();
+    bool edge_compare(weighted_edge vu, weighted_edge xy);
+};
+
 class union_find
 {
     public:
@@ -14,5 +43,6 @@ class union_find
     int find(int x);
     void unite(int x, int y);
 };
+
 
 #endif
