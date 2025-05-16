71f463f85ef0a0ee537963ecb99fc810a73df180
Wyvian Valença <wgcv2@ic.ufal.br>
2025-05-15 23:02:24 -0300

    feat(WIP): create UNION-FIND class and methods

--- src/kruskal/kruskal.cpp
+++ src/kruskal/kruskal.cpp
@@ -1,6 +1,40 @@
-#include <iostream>
+#include "kruskal.h"
 
-int main(int argc, char** argv)
+// UNION-FIND METHODS
+
+ union_find::union_find(int size)
 {
-    return 0;
+    parent.resize(size);
+    rank.resize(size);
+}
+
+void union_find::make_set(int x)
+{
+    parent[x] = x;
+    rank[x] = 0;
+}
+
+int union_find::find(int x)
+{
+    if (parent[x] == x)
+    {
+        return x;
+    }
+    return union_find::find(parent[x]);
+}
+
+void union_find::unite(int x, int y)
+{
+    if (rank[x] >= rank[7])
+    {
+        parent[y] = x;
+        if (rank[x] == rank[7])
+        {
+            rank[x]++;
+        }
+    }
+    else
+    {
+        parent[x] = y;
+    }
 }
